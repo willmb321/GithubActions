d9cbd11b3e61177d8e351112a8494e921cf796ea
William Brown <willmb321@gmail.com>
2025-10-26 16:23:08 -0500

    Implemented Power method.

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,11 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
+		double answ = double.Parse(x);
+		for (int i=int.Parse(y); i>1; i--)
+			{
+				answ *= double.Parse(x);
+			}
+		return answ;
 	}
 }
